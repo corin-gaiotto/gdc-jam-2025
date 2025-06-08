ac3a1c8b628c0c185036bf324904b31a77502507
amcli <andrew2024li@gmail.com>
2025-06-08 15:06:30 -0400

    Customer generation testing

--- Scripts/menu_item.gd
+++ Scripts/menu_item.gd
@@ -9,7 +9,7 @@ class_name MenuItem
 @export var ingredients: Array[String]          # Required ingredients
 #@export var priceMultiplier: float               # Dish multiplier on top of ingredient value
 @export var basePrice: float                    # Base price of dish
-@export var baseTimer: float                    #Base time limit of dish
+@export var baseTimer: float = 5                    #Base time limit of dish
 
 # [Visuals]
 @export var menuItemTexture: SpriteFrames       #How the menu item looks.
