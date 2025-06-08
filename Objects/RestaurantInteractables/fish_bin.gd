cd54c631b699757d0af9cd9152cd8e62e99a39d2
ilikeapple11 <133582503+ilikeapple11@users.noreply.github.com>
2025-06-08 15:09:33 -0400

    bugfixes to fish bin

--- Objects/RestaurantInteractables/fish_bin.gd
+++ Objects/RestaurantInteractables/fish_bin.gd
@@ -13,13 +13,15 @@ func interact(player : RestaurantPlayer):
 		print("[interact] checking fish ", [fishType,storedIngredients[0]])
 		var grabbedFish = FoodItem.new() #[fishType,storedIngredients[0]]
 		grabbedFish.Ingredient = fishType
+		# there is a  rare bug here "Invalid assignment of perpoerty of key 'baseValue' with a type 'Array' on a base object of type'RefCounted (FoodItem)'.
+		print("[interact] grabbing fish from fish bin ", grabbedFish.baseValue, grabbedFish, " ", storedIngredients, " ",storedIngredients[0])
 		grabbedFish.baseValue = storedIngredients[0]
 		storedIngredients.pop_front()
 		player.heldIngredient = grabbedFish
 		print("[interact] grabbed fish ", player.heldIngredient)
 		pass
 	elif player.heldIngredient != null and player.heldIngredient.Ingredient == fishType and !player.heldIngredient.Cooked:
-		storedIngredients.append([player.heldIngredient.Ingredient, player.heldIngredient.baseValue])
+		storedIngredients.append(player.heldIngredient.baseValue)
 		player.heldIngredient = null
 		print("[interact] stored fish in fish bin")
 	
